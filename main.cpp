b57f3569e6fab0a0649c823467dce2e7b939b76b
Ruslan Starovoitov <starovoytov.ruslan@gmail.com>
2019-11-10 13:22:46 +0200

    тест 2

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char **argv)
 {
     QGuiApplication app(argc, argv);
 
-    //Тест работы ветки
+    //Тест работы ветки 2
     //Показ окна
     AnalogClockWindow clock;
     clock.show();
